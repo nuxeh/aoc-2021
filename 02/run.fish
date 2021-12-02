1086c461a36ba0ad0403606847dcf0a78a16deed
Ed Cragg <ed.cragg@eipi.xyz>
2021-12-02 10:49:29 +0000

    Timing
    Part 1
    x=15
    y=10
    150
    
    ________________________________________________________
    Executed in  137.97 millis    fish           external
       usr time  241.15 millis  175.00 micros  240.97 millis
       sys time   35.10 millis    0.00 micros   35.10 millis
    
    x=1823
    y=1018
    1855814
    
    ________________________________________________________
    Executed in    1.95 secs    fish           external
       usr time    3.92 secs  174.00 micros    3.92 secs
       sys time    0.89 secs    0.00 micros    0.89 secs
    
    Part 2
    x=15
    y=60
    900
    
    ________________________________________________________
    Executed in  137.34 millis    fish           external
       usr time  230.17 millis  139.00 micros  230.03 millis
       sys time   45.35 millis   15.00 micros   45.33 millis
    
    x=1823
    y=1012318
    1845455714
    
    ________________________________________________________
    Executed in    1.97 secs    fish           external
       usr time    3.96 secs  210.00 micros    3.96 secs
       sys time    0.90 secs    0.00 micros    0.90 secs

--- 02/run.fish
+++ 02/run.fish
@@ -3,9 +3,9 @@
 aocf checkout 2 2021
 
 echo Part 1
-cat test.txt | ./part1_pre_process.fish | fish
-aocf input | ./part1_pre_process.fish | fish
+time cat test.txt | ./part1_pre_process.fish | fish
+time aocf input | ./part1_pre_process.fish | fish
 
 echo \nPart 2
-cat test.txt | ./part2_pre_process.fish | fish
-aocf input | ./part2_pre_process.fish | fish
+time cat test.txt | ./part2_pre_process.fish | fish
+time aocf input | ./part2_pre_process.fish | fish
