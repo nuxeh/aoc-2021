b465ce2222b5e20478c6abfdaede3727dc260f9b
Ed Cragg <ed.cragg@eipi.xyz>
2021-12-02 09:39:01 +0000

    Part 1

---
+++ 02/part1_pre_process.fish
@@ -0,0 +1,14 @@
+#!/usr/bin/env fish
+
+echo set x 0
+echo set y 0
+
+while read line
+    echo $line | sed 's/forward \([0-9]*\)/set x (math "$x"+\1)/' \
+    | sed 's/down \([0-9]*\)/set y (math "$y"+\1)/' \
+    | sed 's/up \([0-9]*\)/set y (math "$y"-\1)/'
+end
+
+echo 'echo x=$x'
+echo 'echo y=$y'
+echo 'echo (math "$x"\*"$y")'
