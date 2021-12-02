7664d44ecd2f4cc62606261b543a3d4f8e153c97
Ed Cragg <ed.cragg@eipi.xyz>
2021-12-02 00:15:14 +0000

    Part 1 day 1

---
+++ 01/part1.fish
@@ -0,0 +1,10 @@
+set last (aocf input | head -n 1) 
+for num in (aocf input)
+    echo $num
+    if test $num -gt $last
+        set count (math "$count"+1)
+    end
+    set last $num
+end
+echo $count
+
