f1eaae8757315bd61cd04071135c9fdb35e505f6
Ed Cragg <ed.cragg@eipi.xyz>
2021-12-04 13:48:19 +0000

    Parse boards Wip
       [
            [
                255,
                9,
                69,
                63,
                68,
            ],
            [
                36,
                44,
                31,
                35,
                12,
            ],
            [
                39,
                57,
                83,
                6,
                49,
            ],
            [
                23,
                64,
                0,
                10,
                85,
            ],
            [
                81,
                82,
                40,
                34,
                91,
            ],
            [
                24,
                255,
            ],
        ],
    ]

--- 04/src/main.rs
+++ 04/src/main.rs
@@ -1,5 +1,4 @@
 use aocf::Aoc;
-use regex::Regex;
 
 fn main() {
     let mut aoc = Aoc::new()
@@ -28,9 +27,25 @@ fn run(i: &str) {
 
     let boards_stripped = i
         .replace("\n\n", "*")
-        .replace("\n", "")
+        .replace("\n", " ")
         .replace("  ", " ")
         .replace("*", "\n");
 
     println!("boards=\n{}", boards_stripped);
+
+    let boards: Vec<Vec<Vec<u8>>> = boards_stripped
+        .lines()
+        .skip(1) // sequence list
+        .map(|l| l
+            .split(" ")
+            .map(|b| b.parse().unwrap_or(255))
+            .collect::<Vec<u8>>()
+            .chunks(5)
+            .map(|n| n.into())
+            .collect()
+        )
+        .collect();
+
+    println!("boards=\n{:#?}", boards);
+
 }
