c62752c1f7a0de2c421e524068c50a5a4e7e3ea1
Ed Cragg <ed.cragg@eipi.xyz>
2021-12-04 12:05:45 +0000

    Prep d4

---
+++ 04/src/main.rs
@@ -0,0 +1,19 @@
+use aocf::Aoc;
+
+fn main() {
+    let mut aoc = Aoc::new()
+        .year(Some(2021))
+        .day(Some(4))
+        .init()
+        .unwrap();
+
+    let input = aoc.get_input(false);
+
+    if let Ok(i) = input {
+        run(&i);
+    }
+}
+
+fn run(i: &str) {
+
+}
