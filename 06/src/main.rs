98b7eb03c56b22c08b6b3e3d2f2c9a7dea329ab2
Ed Cragg <ed.cragg@eipi.xyz>
2021-12-06 10:27:57 +0000

    ...

--- 06/src/main.rs
+++ 06/src/main.rs
@@ -32,9 +32,10 @@ fn run(input: &str) {
             .iter()
             .map(|fish| {
                 if *fish > 0 {
-                    let newfish = *fish - 1;
+                    let mut newfish = *fish - 1;
                     if newfish == 0 {
-                        add += 1
+                        add += 1;
+                        newfish = 6;
                     }
                     newfish
                 } else {
@@ -43,7 +44,7 @@ fn run(input: &str) {
             })
             .collect();
 
-        (0..add).for_each(|_| initial_state.push(6));
+        (0..add).for_each(|_| initial_state.push(8));
 
         println!("{:2} -> {:?}", day, initial_state);
     }
