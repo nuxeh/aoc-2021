f9f1a54a28fea2fdcdea4f0853bd72bf2d943bdf
Ed Cragg <ed.cragg@eipi.xyz>
2021-12-06 10:08:00 +0000

    Parse input

--- 06/src/main.rs
+++ 06/src/main.rs
@@ -10,14 +10,18 @@ fn main() {
     let input = aoc.get_input(false);
 
     if let Ok(i) = input {
+        println!("{}", i);
         run(&i);
     }
 }
 
 fn run(input: &str) {
     let initial_state: Vec<u8> = input
+        .trim()
         .split(",")
+        .map(|f| {println!("{}", f); f})
         .map(|f| f.parse())
+        .map(|f| {println!("{:?}", f); f})
         .flatten()
         .collect();
 
