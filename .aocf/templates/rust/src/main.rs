4e4fef60331ac45f2b544b1700fae94520c4946e
Ed Cragg <ed.cragg@eipi.xyz>
2021-12-07 10:56:24 +0000

    Add rust template

---
+++ .aocf/templates/rust/src/main.rs
@@ -0,0 +1,16 @@
+use aocf::Aoc;
+
+fn main() {
+    let mut aoc = Aoc::new()
+        .year(Some({{ year }}))
+        .day(Some({{ day }}))
+        .init()
+        .unwrap();
+
+    let input = aoc.get_input(false);
+
+    if let Ok(i) = input {
+        println!("{}", i);
+    }
+}
+
