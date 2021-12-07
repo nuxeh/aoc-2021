38946761d91b88069bbf59300d51dd200d9d0c56
Ed Cragg <ed.cragg@eipi.xyz>
2021-12-07 11:33:27 +0000

    Parse

---
+++ 07/src/main.rs
@@ -0,0 +1,26 @@
+use aocf::Aoc;
+
+fn main() {
+    let mut aoc = Aoc::new()
+        .year(Some(2021))
+        .day(Some(7))
+        .init()
+        .unwrap();
+
+    let input = aoc.get_input(false);
+
+    if let Ok(i) = input {
+        run(&i);
+    }
+}
+
+fn run(i: &str) {
+    let crabs: Vec<u8> = i
+        .trim()
+        .split(",")
+        .map(|c| c.parse())
+        .flatten()
+        .collect();
+
+    println!("{:?}", crabs);
+}
