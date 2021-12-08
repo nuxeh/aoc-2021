5d4e8f07e13f69dd40e8e9f38cd751d2370f5f2c
Ed Cragg <ed.cragg@eipi.xyz>
2021-12-08 11:23:00 +0000

    Parse
        [
            "acbdgf daeb gcbeaf gdfce adefg fbdga cbdfega fae fgdbea ea",
            "ea fcdgaeb ae gecfadb",
        ],
        [
            "ga bdgafe bcaed dcgaebf bacfed acedg gbadec edgfc agd bcga",
            "adg dga agcb fdecgab",
        ],
        [
            "ebdcfg cfbdg ecgabf gcd edfc fdcbeag ebcdag dabgf dc cgebf",
            "cd gcd gacbde cd",
        ],
        [
            "egdfac bf afebdg baf bfdeca bgdf fedga egacb gecbafd gbeaf",
            "bfa fb gfdb fb",
        ],
        [
            "gcafb dc gecd fecbgda cfdbg dfebgc cdf fbegd dcfbea fgedab",
            "edfcbg dgbcf cd dcf",
        ],
        [
            "badf fcagd agcefb dfgcab ba dcbeg cedabgf abgdc acegfd abg",
            "ebfagc dfab ab fcbdeag",
        ],
        [
            "cfgbad gdbfe fe efgdacb cbdge dabgfe egdcaf befa bdgfa egf",
            "decagfb eabf baef fe",
        ],
        [
            "dbfea bcaefdg dcfgeb ag bfceag egfcda becfg fgeba gcab ega",
            "agbfecd aedfcgb gcba ga",
        ],
    ]

---
+++ 08/src/main.rs
@@ -0,0 +1,30 @@
+use aocf::Aoc;
+
+fn main() {
+    let mut aoc = Aoc::new()
+        .year(Some(2021))
+        .day(Some(8))
+        .init()
+        .unwrap();
+
+    let input = aoc.get_input(false);
+
+    if let Ok(i) = input {
+        run(&i);
+    }
+}
+
+struct Line<'a> {
+    input: Vec<&'a str>,
+    output: Vec<&'a str>,
+}
+
+fn run(i: &str) {
+    let lines: Vec<Vec<&str>> = i
+        .trim()
+        .lines()
+        .map(|l| l.split(" | ").collect())
+        .collect();
+
+    println!("{:#?}", lines);
+}
